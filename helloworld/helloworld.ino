e8ec091eb8a2f33e5b80f5c2ce531925f3f7d452
Muhammad Alasgarli <Muhammad Alasgarli@DESKTOP-KV28LPK.dhcp.wustl.edu>
2018-01-17 17:22:54 -0600

    Studio 0 Complete

--- helloworld/helloworld.ino
+++ helloworld/helloworld.ino
@@ -4,10 +4,29 @@
  *
  */
 
+
+
+//pinMode(13,OUTPUT);
+//void setup() {
+//
+//Serial.begin(9600);
+//Serial.println("Hello, world!");
+//
+//void loop() {
+
+ 
+ int i=0;
 void setup() {
+   
   Serial.begin(9600);
   Serial.println("Hello, world!");
+  
 }
 
 void loop() {
+
+  Serial.println(i++,DEC);
+  delay(1000);
+  
+  
 }
